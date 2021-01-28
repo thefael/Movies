1b6fcae6b10c3f3478989806516adc66d8767b51
Rafael Rodrigues <rafael-rodrigues-@live.com>
2021-01-28 16:41:19 -0300

    Create Colors file

---
+++ thefaelMovie/Application/Colors.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+enum Colors {
+    static let darkBlue = UIColor(red: 0.1, green: 0.15, blue: 0.3, alpha: 1.0)
+    static let darkestBlue = UIColor(red: 0.05, green: 0.05, blue: 0.2, alpha: 1.0)
+    static let yellow = UIColor(red: 1, green: 0.73, blue: 0, alpha: 1.0)
+}
---
+++ thefaelMovie/Application/ImageCache.swift
@@ -0,0 +1,9 @@
+//
+//  ImageCache.swift
+//  thefaelMovie
+//
+//  Created by Rafael Rodrigues on 27/01/21.
+//  Copyright © 2021 Rafael Rodrigues. All rights reserved.
+//
+
+import Foundation
