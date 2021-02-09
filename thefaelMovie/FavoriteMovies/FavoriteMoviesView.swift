814e835792b525a335cc562e224dc82f5f6f40ff
Rafael Rodrigues <rafael-rodrigues-@live.com>
2021-02-09 19:53:54 -0300

    Create favorite movie cell

---
+++ thefaelMovie/FavoriteMovies/FavoriteMoviesView.swift
@@ -0,0 +1,9 @@
+//
+//  FavoriteMoviesView.swift
+//  thefaelMovie
+//
+//  Created by Rafael Rodrigues on 09/02/21.
+//  Copyright © 2021 Rafael Rodrigues. All rights reserved.
+//
+
+import Foundation
---
+++ thefaelMovie/FavoriteMovies/FavoriteMoviesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FavoriteMoviesViewController.swift
+//  thefaelMovie
+//
+//  Created by Rafael Rodrigues on 09/02/21.
+//  Copyright © 2021 Rafael Rodrigues. All rights reserved.
+//
+
+import Foundation
