fb88223837aac80bde6957df2e890946bacfd608
Rafael Rodrigues <rafael-rodrigues-@live.com>
2021-02-09 21:38:56 -0300

    Create favorite movies view and view controller

--- thefaelMovie/FavoriteMovies/FavoriteMoviesView.swift
+++ thefaelMovie/FavoriteMovies/FavoriteMoviesView.swift
@@ -1,9 +1,29 @@
-//
-//  FavoriteMoviesView.swift
-//  thefaelMovie
-//
-//  Created by Rafael Rodrigues on 09/02/21.
-//  Copyright © 2021 Rafael Rodrigues. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class FavoriteMoviesView: UIView {
+    var collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
+    private let layout = UICollectionViewFlowLayout()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func createFlowLayout() {
+        let width = Constants.screen.width
+        let height = width / 3
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: width, height: height)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+    }
+
+    func setupView() {
+        collectionView = UICollectionView(frame: Constants.screen, collectionViewLayout: layout)
+        collectionView.backgroundColor = Color.darkBlue
+        addSubview(collectionView)
+    }
+}
--- thefaelMovie/FavoriteMovies/FavoriteMoviesViewController.swift
+++ thefaelMovie/FavoriteMovies/FavoriteMoviesViewController.swift
@@ -1,9 +1,26 @@
-//
-//  FavoriteMoviesViewController.swift
-//  thefaelMovie
-//
-//  Created by Rafael Rodrigues on 09/02/21.
-//  Copyright © 2021 Rafael Rodrigues. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class FavoriteMoviesViewController: UIViewController {
+    let favoriteMoviesView = FavoriteMoviesView(frame: Constants.screen)
+    let dataSource = CollectionViewDataSource<PopularMovie, UICollectionViewCell>()
+    var favoriteMoviesList = [PopularMovie]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.dataSource.items = self.favoriteMoviesList
+            }
+        }
+    }
+
+    override func loadView() {
+        view = favoriteMoviesView
+        favoriteMoviesView.createFlowLayout()
+        favoriteMoviesView.setupView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        favoriteMoviesView.collectionView.register(FavoriteMovieCell.self, forCellWithReuseIdentifier: Constants.reuseIdentifier)
+        favoriteMoviesView.collectionView.dataSource = dataSource
+        favoriteMoviesView.collectionView.reloadData()
+    }
+}
